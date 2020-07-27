a71263d0655ee7bd582516b729c0e06b375dd994
gangslee <37660333+gangslee@users.noreply.github.com>
2020-07-27 18:33:16 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const countModifier = () => {};
+const countModifier = (count = 0, action) => {
+  if (action.type === 'Add') {
+    return count + 1;
+  } else if (action.type === 'Minus') {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
 
 const countStore = createStore(countModifier);
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+countStore.dispatch({type: 'Add'});
+countStore.dispatch({type: 'Add'});
+countStore.dispatch({type: 'Add'});
+countStore.dispatch({type: 'Add'});
+countStore.dispatch({type: 'Minus'});
+
+console.log(countStore.getState());
